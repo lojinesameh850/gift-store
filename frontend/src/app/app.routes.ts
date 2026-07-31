cccd3f1cf45839e77a01a3e174f9e745491b0bf8
Logy <lojinesameh850@gmail.com>
2026-07-31 21:38:05 +0300

    Account management front integrated with back

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { profileComponent } from './pages/account/profilePage';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Redirect root URL directly to account page FOR NOW
+  { path: '', redirectTo: 'account/profile', pathMatch: 'full' },
+
+  // Customer profile route
+  { path: 'account/profile', component: profileComponent },
+
+  // Fallback for unknown routes
+  { path: '**', redirectTo: 'error' }
+];
