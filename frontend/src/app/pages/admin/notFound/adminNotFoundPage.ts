df2e32d35db16c24dbc105ac677d61072c5da8cc
Logy <lojinesameh850@gmail.com>
2026-08-05 18:21:11 +0300

    Implemented error pages

---
+++ frontend/src/app/pages/admin/notFound/adminNotFoundPage.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-not-found',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './adminNotFoundPage.html',
+  styleUrls: ['./adminNotFoundPage.css']
+})
+export class adminNotFoundComponent {}
